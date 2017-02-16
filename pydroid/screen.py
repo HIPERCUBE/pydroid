c3d6e63e1bbb427bfc2d7325d262e20f5c7bcb26
Joowon Ryoo <joowonryoo@gmail.com>
2017-02-17 00:15:20 +0900

    screen interface

---
+++ pydroid/screen.py
@@ -0,0 +1,51 @@
+import _adb
+
+
+def get_size():
+    # adb shell wm size
+    pass
+
+
+def get_width():
+    pass
+
+
+def get_height():
+    pass
+
+
+def set_size():
+    # adb shell wm size 1440x2560
+    pass
+
+
+def set_width():
+    pass
+
+
+def set_height():
+    pass
+
+
+def get_density():
+    # adb shell wm density
+    pass
+
+
+def set_density():
+    # adb shell wm density 160
+    pass
+
+
+def screen_shot(output):
+    # adb shell screencap
+    pass
+
+
+def start_record():
+    # adb shell screenrecord /sdcard/asdf
+    pass
+
+
+def stop_record(output):
+    pass
